870d86e426314fd4497c6d2c7e21399fea0c3621
lucasw <wsacul@gmail.com>
2013-05-03 23:06:38 -0700

    Playing with visualization, TBD make a set of graphs for all buttons and axes.

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main( int argc, char* argv[] )
   char name[NAME_LENGTH] = "Unknown";
 
   if ((fd = open("/dev/input/js0", O_RDONLY)) < 0) {
-    perror("jstest");
+    perror("controlrecorder");
     exit(1);
   }
 
@@ -72,10 +72,6 @@ int main( int argc, char* argv[] )
   ioctl(fd, JSIOCGNAME(NAME_LENGTH), name);
 
 
-  LOG(INFO) << "Joystick (" << name << ") has " 
-      << axes << " axes and " << buttons << " buttons. Driver version is "
-      << (version >> 16) << "."  << ((version >> 8) & 0xff) << "."
-      << (version & 0xff);
 
   struct js_event js;
 
@@ -89,13 +85,59 @@ int main( int argc, char* argv[] )
   axis.resize(axes);
   button.resize(buttons);
 
-  while (1) {
+  const bool write_output = false;
 
+  cv::FileStorage fs;
+  if (write_output) {
+  fs.open("joystick.yml", cv::FileStorage::WRITE);
+
+  time_t rawtime; time(&rawtime);
+  fs << "date" << asctime(localtime(&rawtime));
+
+  LOG(INFO) << "Joystick (" << name << ") has " 
+      << axes << " axes and " << buttons << " buttons. Driver version is "
+      << (version >> 16) << "."  << ((version >> 8) & 0xff) << "."
+      << (version & 0xff);
+
+  fs << "name" << name;
+  fs << "axes" << axes;
+  fs << "buttons" << buttons;
+  fs << "version" << version;
+
+  fs << "events" << "[";
+  }
+
+  cv::Point2f old;
+  cv::Point2f pos = cv::Point2f(im.cols/2, im.rows/2);
+  cv::Point2f vel;
+
+  int ind = 1000000;
+  bool do_loop = true;
+  while (do_loop) {
+
+    old = pos;
     int i = 0;
-    while ((read(fd, &js, sizeof(struct js_event)) == sizeof(struct js_event)) && (i< 50))  {
+    while ((read(fd, &js, sizeof(struct js_event)) == sizeof(struct js_event)) 
+        && (i < 25))  {
       //LOG(INFO) << "Event: type " << js.type << ", time " << js.time
       //    << ", number " << js.number << ", value " << js.value;
-      
+
+      if (errno != EAGAIN) {
+        perror("\ncontrolrecorder: error reading");
+        //do_loop = false;
+        //break;
+      }
+
+      if (write_output) {
+        fs << "{:";
+        fs << "ind" << ind;
+        fs << "time" << (int)js.time;
+        fs << "type" << (int)js.type;
+        fs << "number" << (int)js.number;
+        fs << "value" << (int) js.value;
+        fs << "}";
+      }
+      ind++;
       i++;
       switch(js.type & ~JS_EVENT_INIT) {
           case JS_EVENT_BUTTON:
@@ -105,11 +147,45 @@ int main( int argc, char* argv[] )
           axis[js.number] = js.value;
           break;
       }
- 
-          cv::Point2f pt = cv::Point2f( 
-              im.cols*((float)axis[0] + 32768.0)/(65536.0),
-              im.rows*((float)axis[1] + 32768.0)/(65536.0));
 
+    }
+
+    {
+      float fr = 2.25;
+      float raw_x = ((float)axis[0])/(32768.0);
+      float sign_x = (raw_x > 0) * 2.0 - 1.0;
+      raw_x *= fr;
+      raw_x *= raw_x * sign_x;
+
+      vel.x += raw_x;
+
+      float raw_y = ((float)axis[1])/(32768.0);
+      float sign_y = (raw_y > 0) * 2.0 - 1.0;
+      raw_y *= fr;
+      raw_y *= raw_y;
+      vel.y += raw_y * sign_y;
+
+      fr = 0.65;
+      pos.x += vel.x;
+      pos.y += vel.y;
+      vel.x *= fr;
+      vel.y *= fr;
+      if (pos.x > im.cols) {
+        pos.x = im.cols;
+        vel.x = 0;
+      }
+      if (pos.y > im.rows) {
+        pos.y = im.rows;
+        vel.y = 0;
+      }
+      if (pos.x < 0) {
+        pos.x = 0;
+        vel.x = 0;
+      }
+      if (pos.y < 0) {
+        pos.y = 0;
+        vel.y = 0;
+      }
           //LOG(INFO) << pt.x << " " << pt.y;
 
           cv::Scalar col = cv::Scalar(
@@ -118,22 +194,20 @@ int main( int argc, char* argv[] )
               230
               );
 
-          cv::circle(im,
-              pt,
-              3,
-              col
+          cv::line(im,
+              old,
+              pos,
+              col,
+              4,4
               );   
-    }
-
-    if (errno != EAGAIN) {
-      perror("\njstest: error reading");
-      exit (1);
     }
 
 
     cv::imshow("im", im);
     char d = cv::waitKey(100);
-    if (d == 'q') exit(0);
+    if (d == 'q') { do_loop = false; };
   }
-
+  
+  fs << "]";
+  fs.release();
 }
